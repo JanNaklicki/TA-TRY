62a1816dcb1274c121f3b44ae4def3049cb45380
JanNaklicki <janek.naklicki@gmail.com>
2024-01-05 03:23:20 +0100

    Implemented algorithm

--- src/graphmodule/graphclass.py
+++ src/graphmodule/graphclass.py
@@ -94,7 +94,7 @@ class GraphClass:
             for i in range(len(way_nodes) - 1):
                 self.graph.add_edge(way_nodes[i], way_nodes[i + 1], weight=5) #TODO: change it so it will get time here
 
-    def h(self, n, goal):
+    def h(self, n):
         H = {
             'A': 1,
             'B': 30,
@@ -105,7 +105,88 @@ class GraphClass:
 
         return 1
 
+    def get_neighbors(self, v):
+        return list(self.graph.edges(v, data=True))
+
+
     def a_star_algorithm(self, start_node, stop_node):
+        # open_list is a list of nodes which have been visited, but who's neighbors
+        # haven't all been inspected, starts off with the start node
+        # closed_list is a list of nodes which have been visited
+        # and who's neighbors have been inspected
+        open_list = set([start_node])
+        closed_list = set([])
+
+        # g contains current distances from start_node to all other nodes
+        # the default value (if it's not found in the map) is +infinity
+        g = {}
+
+        g[start_node] = 0
+
+        # parents contains an adjacency map of all nodes
+        parents = {}
+        parents[start_node] = start_node
+
+        while len(open_list) > 0:
+            n = None
+
+            # find a node with the lowest value of f() - evaluation function
+            for v in open_list:
+                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+                    n = v
+
+            if n == None:
+                print('Path does not exist!')
+                return None
+
+            # if the current node is the stop_node
+            # then we begin reconstructin the path from it to the start_node
+            if n == stop_node:
+                reconst_path = []
+
+                while parents[n] != n:
+                    reconst_path.append(n)
+                    n = parents[n]
+
+                reconst_path.append(start_node)
+
+                reconst_path.reverse()
+
+                print('Path found: {}'.format(reconst_path))
+                return reconst_path
+
+            # for all neighbors of the current node do
+            for data in self.get_neighbors(n):
+                # if the current node isn't in both open_list and closed_list
+                # add it to open_list and note n as it's parent
+                neighbour = data[1]
+                weight = data[2]['weight']
+                if neighbour not in open_list and neighbour not in closed_list:
+                    open_list.add(neighbour)
+                    parents[neighbour] = n
+                    g[neighbour] = g[n] + weight
+
+                # otherwise, check if it's quicker to first visit n, then m
+                # and if it is, update parent data and g data
+                # and if the node was in the closed_list, move it to open_list
+                else:
+                    if g[neighbour] > g[n] + weight:
+                        g[neighbour] = g[n] + weight
+                        parents[neighbour] = n
+
+                        if neighbour in closed_list:
+                            closed_list.remove(neighbour)
+                            open_list.add(neighbour)
+
+            # remove n from the open_list, and add it to closed_list
+            # because all of his neighbors were inspected
+            open_list.remove(n)
+            closed_list.add(n)
+
+        print('Path does not exist!')
+        return None
+
+    def a_star_algorithm_package(self, start_node, stop_node):
         if start_node not in self.graph.nodes or stop_node not in self.graph.nodes:
             print('Start or stop node not in the graph!')
             return None
@@ -176,80 +257,80 @@ from collections import deque
 #
 #         return H[n]
 #
-#     def a_star_algorithm(self, start_node, stop_node):
-#         # open_list is a list of nodes which have been visited, but who's neighbors
-#         # haven't all been inspected, starts off with the start node
-#         # closed_list is a list of nodes which have been visited
-#         # and who's neighbors have been inspected
-#         open_list = set([start_node])
-#         closed_list = set([])
-#
-#         # g contains current distances from start_node to all other nodes
-#         # the default value (if it's not found in the map) is +infinity
-#         g = {}
-#
-#         g[start_node] = 0
-#
-#         # parents contains an adjacency map of all nodes
-#         parents = {}
-#         parents[start_node] = start_node
-#
-#         while len(open_list) > 0:
-#             n = None
-#
-#             # find a node with the lowest value of f() - evaluation function
-#             for v in open_list:
-#                 if n == None or g[v] + self.h(v) < g[n] + self.h(n):
-#                     n = v;
-#
-#             if n == None:
-#                 print('Path does not exist!')
-#                 return None
-#
-#             # if the current node is the stop_node
-#             # then we begin reconstructin the path from it to the start_node
-#             if n == stop_node:
-#                 reconst_path = []
-#
-#                 while parents[n] != n:
-#                     reconst_path.append(n)
-#                     n = parents[n]
-#
-#                 reconst_path.append(start_node)
-#
-#                 reconst_path.reverse()
-#
-#                 print('Path found: {}'.format(reconst_path))
-#                 return reconst_path
-#
-#             # for all neighbors of the current node do
-#             for (m, weight) in self.get_neighbors(n):
-#                 # if the current node isn't in both open_list and closed_list
-#                 # add it to open_list and note n as it's parent
-#                 if m not in open_list and m not in closed_list:
-#                     open_list.add(m)
-#                     parents[m] = n
-#                     g[m] = g[n] + weight
-#
-#                 # otherwise, check if it's quicker to first visit n, then m
-#                 # and if it is, update parent data and g data
-#                 # and if the node was in the closed_list, move it to open_list
-#                 else:
-#                     if g[m] > g[n] + weight:
-#                         g[m] = g[n] + weight
-#                         parents[m] = n
-#
-#                         if m in closed_list:
-#                             closed_list.remove(m)
-#                             open_list.add(m)
-#
-#             # remove n from the open_list, and add it to closed_list
-#             # because all of his neighbors were inspected
-#             open_list.remove(n)
-#             closed_list.add(n)
-#
-#         print('Path does not exist!')
-#         return None
+    # def a_star_algorithm(self, start_node, stop_node):
+    #     # open_list is a list of nodes which have been visited, but who's neighbors
+    #     # haven't all been inspected, starts off with the start node
+    #     # closed_list is a list of nodes which have been visited
+    #     # and who's neighbors have been inspected
+    #     open_list = set([start_node])
+    #     closed_list = set([])
+    #
+    #     # g contains current distances from start_node to all other nodes
+    #     # the default value (if it's not found in the map) is +infinity
+    #     g = {}
+    #
+    #     g[start_node] = 0
+    #
+    #     # parents contains an adjacency map of all nodes
+    #     parents = {}
+    #     parents[start_node] = start_node
+    #
+    #     while len(open_list) > 0:
+    #         n = None
+    #
+    #         # find a node with the lowest value of f() - evaluation function
+    #         for v in open_list:
+    #             if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+    #                 n = v;
+    #
+    #         if n == None:
+    #             print('Path does not exist!')
+    #             return None
+    #
+    #         # if the current node is the stop_node
+    #         # then we begin reconstructin the path from it to the start_node
+    #         if n == stop_node:
+    #             reconst_path = []
+    #
+    #             while parents[n] != n:
+    #                 reconst_path.append(n)
+    #                 n = parents[n]
+    #
+    #             reconst_path.append(start_node)
+    #
+    #             reconst_path.reverse()
+    #
+    #             print('Path found: {}'.format(reconst_path))
+    #             return reconst_path
+    #
+    #         # for all neighbors of the current node do
+    #         for (m, weight) in self.get_neighbors(n):
+    #             # if the current node isn't in both open_list and closed_list
+    #             # add it to open_list and note n as it's parent
+    #             if m not in open_list and m not in closed_list:
+    #                 open_list.add(m)
+    #                 parents[m] = n
+    #                 g[m] = g[n] + weight
+    #
+    #             # otherwise, check if it's quicker to first visit n, then m
+    #             # and if it is, update parent data and g data
+    #             # and if the node was in the closed_list, move it to open_list
+    #             else:
+    #                 if g[m] > g[n] + weight:
+    #                     g[m] = g[n] + weight
+    #                     parents[m] = n
+    #
+    #                     if m in closed_list:
+    #                         closed_list.remove(m)
+    #                         open_list.add(m)
+    #
+    #         # remove n from the open_list, and add it to closed_list
+    #         # because all of his neighbors were inspected
+    #         open_list.remove(n)
+    #         closed_list.add(n)
+    #
+    #     print('Path does not exist!')
+    #     return None
 #
 #
 #
